12166b7ddacb1dabaa075fc44374e10b2bd1cd41
guilchap <guillaume.cha.g@gmail.com>
2022-11-17 08:24:26 +0100

    add web Dijon

--- raspberry/dijon/conf_apache.sh
+++ raspberry/dijon/conf_apache.sh
@@ -2,6 +2,7 @@
 
 LOG_FILE_WEB="/var/log/datacenter/conf_web.log"
 
+CITY="Dijon"
 
 echo "---------------------"
 echo "START - Configure Web"
@@ -10,7 +11,7 @@ echo "=> [1]: Delete default file"
 rm /var/www/html/index.html
 
 echo "=> [2]: Copy new files"
-cp -R /tmp/pfe-demo/data/site-html/* /var/www/html/
+cp -R /tmp/pfe-demo/data/site-html/$CITY/* /var/www/html/
 
 echo "=> [3]: Restart service"
 service apache2 reload
