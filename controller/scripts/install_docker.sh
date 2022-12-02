28a209bea16e82b48fe4cd31b7939ced33f6c06d
Elodie <elodie.apiou@reseau.eseo.fr>
2022-12-02 15:28:16 +0100

    ajout docker

---
+++ controller/scripts/install_docker.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#remove les anciennes versions peut être présente
+
+echo "-----------------------"
+echo "[1] => Remove old Docker"
+
+apt-get remove docker docker-engine docker.io containerd runc
+
+#installation de docker sur la machine, avec ses dépendances
+
+echo "[2] => Install Docker"
+
+apt-get update
+apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+
+chmod a+r /etc/apt/keyrings/docker.gpg
+apt-get update
+
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
