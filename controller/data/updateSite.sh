eae5ec067fd3d67a3149b089330515ca1fa7af2f
guilchap <guillaume.cha.g@gmail.com>
2023-01-06 10:57:08 +0100

    update site script

--- controller/data/updateSite.sh
+++ controller/data/updateSite.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd /opt/tomcat/apache-tomcat-9.0.69/webapps/
+
 echo "Vous êtes bien dans le webapps de tomcat"
-rm pfe-demo-css.war
+rm /opt/tomcat/apache-tomcat-9.0.70/webapps/pfe-demo-css.war
 echo "Le point war du site a bien été remove"
-rm -r pfe-demo-css/
+rm -r /opt/tomcat/apache-tomcat-9.0.70/webapps/pfe-demo-css/
 echo "Le site a bien été remove"
-cp /vagrant/data/pfe-demo-css.war .
+cp /vagrant/data/pfe-demo-css.war /opt/tomcat/apache-tomcat-9.0.70/webapps/
 echo "Le nouveau site est en place"
