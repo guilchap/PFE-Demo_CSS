3255c087d6b72d602d59422da4a54cff54ddf28b
guilchap <guillaume.cha.g@gmail.com>
2023-01-13 15:52:49 +0100

    fix conf wifi

--- raspberry/wifi-hotspot/conf_wifi.sh
+++ raspberry/wifi-hotspot/conf_wifi.sh
@@ -15,32 +15,13 @@ echo "----------------------"
 echo "START - Configure WiFi"
 
 echo "=> [1]: Installing required packages..."
-# apt-get update $APT_OPT \
-#     >> $LOG_FILE_WIFI 2>&1
-# apt-get upgrade $APT_OPT \
-#     >> $LOG_FILE_WIFI 2>&1    
-
 apt-get install \
     network-manager \
     >> $LOG_FILE_WIFI 2>&1
 systemctl start NetworkManager.service 
 systemctl enable NetworkManager.service
 
-
-# echo "=> [2]: Configure interface wlan"
-# echo "# interface $INT_NAME" >> /etc/dhcpcd.conf
-# echo "interface $INT_NAME" >> /etc/dhcpcd.conf
-# echo "  static ip_address=$IP_ADDRESS" >> /etc/dhcpcd.conf
-# service dhcpcd restart
-
-
-echo "=> [3]: Configure DHCP"
-mv /etc/dnsmasq.conf /etc/dnsmasq.conf.old
-echo "interface=$INT_NAME" >> /etc/dnsmasq.conf
-echo "dhcp-range=$DHCP_START,$DHCP_END,$DHCP_MASK,$DHCP_LENGTH" >> /etc/dnsmasq.conf
-
-
-echo "=> [4]: Configure Hotspot"
+echo "=> [2]: Configure Hotspot"
 nmcli con add type wifi ifname $INT_NAME con-name $WIFI_NAME autoconnect yes ssid $WIFI_NAME
 nmcli con modify $WIFI_NAME ipv4.addresses 192.168.25.1/24 
 nmcli con modify $WIFI_NAME 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
