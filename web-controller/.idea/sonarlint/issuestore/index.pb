992f603540a1b3ad080ddb4cae2afef8fe2fd99f
Dorian <dorian.camilleri@reseau.eseo.fr>
2023-01-18 23:55:59 +0100

    Mise à jour site web admin

---
+++ web-controller/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+R
+"src/main/java/com/Application.java,0\6\060e4465fa508961e9fd44875c5a850a9925ebfe
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
--- web-controller/src/main/java/com/controller/ApplicationController.java
+++ web-controller/src/main/java/com/controller/ApplicationController.java
@@ -1,13 +1,20 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class ApplicationController {
@@ -38,7 +45,7 @@ public class ApplicationController {
     public void startScript() throws IOException {
         ProcessBuilder pb = new ProcessBuilder();
         try{
-            pb.command("sh", "/vagrant/data/startRasp.sh");
+            pb.command("sh", "/vagrant/data/checkRasp.sh");
             pb.directory(new File(System.getProperty("user.home")));
             Process p = pb.start();
             try (var reader = new BufferedReader(
@@ -64,6 +71,66 @@ public class ApplicationController {
     @GetMapping("/infra")
     public String infra(){return "homeInfra";}
 
+    @PostMapping("/infra")
+    public String stopInfra(Model model){
+        String result = "";
+        ProcessBuilder pb = new ProcessBuilder();
+        try{
+            pb.command("sh", "/vagrant/data/stopDocker.sh");
+            pb.directory(new File(Paths.get("/vagrant/data").toString()));
+            Process p = pb.start();
+            try (var reader = new BufferedReader(
+                    new InputStreamReader(p.getInputStream()))) {
+
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+
+            }
+            result = "success";
+        }catch (Exception e){
+            System.out.println("Le fichier spécifié est introuvable");
+            result = "failure";
+        }
+        model.addAttribute("result", result);
+        return "homeInfra";
+    }
+
     @GetMapping("/infraweb")
-    public String infraWeb(){return "webClients";}
+    public String infraWeb(Model model){
+        String result = "";
+        List<List<String>> outputList = new ArrayList<>();
+        ProcessBuilder pb = new ProcessBuilder();
+        try{
+            pb.command("docker", "ps");
+            pb.directory(new File(Paths.get("/vagrant/data").toString()));
+            Process p = pb.start();
+            try (var reader = new BufferedReader(
+                    new InputStreamReader(p.getInputStream()))) {
+
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    String[] lineSplit = line.split("( {2,})");
+                    List<String> list = Arrays.asList(lineSplit);
+                    list = list.stream().filter(s -> !s.isEmpty()).collect(Collectors.toList());
+                    System.out.println(line);
+                    System.out.println(list.get(5));
+                    if(!list.get(0).equals("CONTAINER ID")){
+                        outputList.add(list);
+                    }
+                }
+
+            }
+            result = "success";
+        }catch (Exception e){
+            System.out.println("Le fichier spécifié est introuvable");
+            result = "failure";
+        }
+        model.addAttribute("outputList", outputList);
+        model.addAttribute("result", result);
+        return "webClients";
+    }
 }
