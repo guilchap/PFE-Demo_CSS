880c079af6a7a06d646d539ada231948661d291c
guilchap <guillaume.cha.g@gmail.com>
2023-01-18 19:07:37 +0100

    Update checkRasp.sh

--- controller/data/checkRasp.sh
+++ controller/data/checkRasp.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-ping 192.168.25.2 -c 4 >> /vagrant/logs/output_ping_angers.log && ping 192.168.25.3 -c 4 >> /vagrant/logs/output_ping_dijon.log && whoami
+ping 192.168.25.2 -c 4 >> /vagrant/logs/output_ping_angers.log && ping 192.168.25.3 -c 4 >> /vagrant/logs/output_ping_dijon.log
