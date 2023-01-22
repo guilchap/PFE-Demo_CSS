89ba388f5b1d7f5e94b308b42c53705a647e5ee2
guilchap <guillaume.cha.g@gmail.com>
2023-01-22 21:26:30 +0100

    fix proxy default site

--- controller/scripts/install_tomcat.sh
+++ controller/scripts/install_tomcat.sh
@@ -8,36 +8,36 @@ LOG_FILE="/vagrant/logs/install_tomcat.log"
 echo "----------------------"
 echo "START - install Tomcat"
 
-echo "=> [2]: Add new user"
+echo "=> [1]: Add new user"
 useradd -r -m -U -d /opt/tomcat -s /bin/false tomcat
 usermod -aG docker tomcat
 
-echo "=> [3]: Download and extract Tomcat"
+echo "=> [2]: Download and extract Tomcat"
 wget -O /tmp/tomcat.tar.gz -c https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.71/bin/apache-tomcat-9.0.71.tar.gz \
     >> $LOG_FILE 2>&1
 tar xf /tmp/tomcat.tar.gz -C /opt/tomcat
 
-echo "=> [4]: Folders management"
+echo "=> [3]: Folders management"
 ln -s /opt/tomcat/apache-tomcat-9.0.71 /opt/tomcat/updated
 
 chown -R tomcat: /opt/tomcat/*
 
 sh -c 'chmod +x /opt/tomcat/updated/bin/*.sh'
 
-echo "=> [5]: Add new files"
+echo "=> [4]: Add new files"
 cp /vagrant/data/tomcat.service /etc/systemd/system/tomcat.service
 cp /vagrant/data/pfe-demo-css.war /opt/tomcat/apache-tomcat-9.0.71/webapps/
 cp /vagrant/data/etudiant.war /opt/tomcat/apache-tomcat-9.0.71/webapps/
 
-echo "=> [6]: Add path to access"
+echo "=> [5]: Add path to access"
 sed -i '159i\	<Context path="" docBase="/opt/tomcat/apache-tomcat-9.0.71/webapps/etudiant" reloadable="true"/>' /opt/tomcat/apache-tomcat-9.0.71/conf/server.xml
 sed -i '160i\	<Context path="/admin" docBase="/opt/tomcat/apache-tomcat-9.0.71/webapps/pfe-demo-css" />' /opt/tomcat/apache-tomcat-9.0.71/conf/server.xml
 
-echo "=> [7]: Network management"
+echo "=> [6]: Network management"
 ufw allow 8080/tcp
 echo '192.168.25.5 demo.css.eseo'>> /etc/hosts
 
-echo "=> [8]: Start Tomcat service"
+echo "=> [7]: Start Tomcat service"
 systemctl daemon-reload
 systemctl start tomcat
 systemctl enable tomcat
